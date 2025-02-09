ec6c16def6d1e7359a9b88e638a515e6d0648004
704hj <704hj@naver.com>
2025-02-09 23:39:52 +0900

    다크모드 사진, 크기 수정

--- frontend/src/components/DarkMode.js
+++ frontend/src/components/DarkMode.js
@@ -2,13 +2,12 @@ import React from "react";
 import "./DarkMode.css";
 
 const DarkMode = ({ darkMode, setDarkMode }) => {
-    const imageSrc = darkMode
+  const imageSrc = darkMode
     ? `${process.env.PUBLIC_URL}/images/dark.png`
     : `${process.env.PUBLIC_URL}/images/light.png`;
 
-
   return (
-    <button onClick={() => setDarkMode(!darkMode)}>
+    <button className="dark-mode-toggle" onClick={() => setDarkMode(!darkMode)}>
       <img src={imageSrc} alt={darkMode ? "다크 모드" : "라이트 모드"} />
     </button>
   );
