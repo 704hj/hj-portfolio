51d4ad05e2c01d292d48993524620f365e8bab87
704hj <704hj@naver.com>
2025-02-14 00:16:49 +0900

    aboutme 반응형 수정

--- frontend/src/components/Navbar/SideNav.js
+++ frontend/src/components/Navbar/SideNav.js
@@ -2,12 +2,21 @@ import React from "react";
 import "./SideNav.css";
 
 const SideNav = () => {
+  const handleScroll = (e, targetId) => {
+    e.preventDefault(); // 기본 이벤트 방지
+    const targetElement = document.getElementById(targetId); // 해당 id 찾기
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth", block: "start" }); // 스크롤 부드럽게
+    }
+  };
+
   return (
     <nav className="side-nav">
       <ul>
-        <li><a href="#home"><img src="/images/home.png" alt="Home" /></a></li>
-        <li><a href="#about"><img src="/images/aboutme.png" alt="About" /></a></li>
-        <li><a href="#project"><img src="/images/project.png" alt="Project" /></a></li>
+        <li><a href="#home" onClick={(e) => handleScroll(e, "home")}><img src="/images/home.png" alt="Home" /></a></li>
+        <li><a href="#about" onClick={(e) => handleScroll(e, "about")}><img src="/images/aboutme.png" alt="About" /></a></li>
+        <li><a href="#project" onClick={(e) => handleScroll(e, "project")}><img src="/images/project.png" alt="Project" /></a></li>
       </ul>
     </nav>
   );
